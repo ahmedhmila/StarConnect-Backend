c1c147791a1002d9a622ca22ff55f3b287808015
HMILA Ahmed <ahmed.hmila@littlebigconnection.com>
2025-12-05 04:47:54 +0100

    Upload pre-built admin panel

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,8 +4,8 @@ export default ({ env }) => ({
       provider: 'strapi-provider-upload-azure-storage-v5',
       providerOptions: {
         account: env('AZURE_STORAGE_ACCOUNT'),
-        accountKey: env('AZURE_STORAGE_ACCOUNT_KEY'),
-        containerName: env('AZURE_STORAGE_CONTAINER'),
+        accountKey: env('AZURE_STORAGE_ACCESS_KEY'),
+        containerName: env('AZURE_STORAGE_CONTAINER_NAME'),
         defaultPath: env('AZURE_STORAGE_DEFAULT_PATH', 'assets'),
         serviceBaseURL: env(
           'AZURE_STORAGE_URL',
